b38068222c7846154f8d0a0e49cd26ae4a347e4e
Abdelwahab <ab28fb@gmail.com>
2024-01-17 18:07:54 +0100

    INIT and installing firebase dependancies

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChzr_DE3geVCwLYDdNR7xs_iiGMctB084",
+  authDomain: "moalime-chat-temp.firebaseapp.com",
+  projectId: "moalime-chat-temp",
+  storageBucket: "moalime-chat-temp.appspot.com",
+  messagingSenderId: "1079527028303",
+  appId: "1:1079527028303:web:3f6ed3eb1877aeb43b9c3a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
